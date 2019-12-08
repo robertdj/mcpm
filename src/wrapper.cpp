83dc984a13c7cbc49216c9cf00a41fb71f7a174d
Robert DJ <git@dahl-jacobsen.dk>
2019-12-08 19:24:55 +0100

    Get input to C++ directly from R

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -1,59 +1,30 @@
 #include <Rcpp.h>
 using namespace Rcpp;
-#include <string>
+
 #include "Matching.h"
 #include "Graph.h"
 
-#include <fstream>
-#include <cstdlib>
-#include <iostream>
-// #include <string>
-#include <sstream>
-using namespace std;
+pair< Graph, vector<double> > CreateGraph(int nvertices, int nedges, Rcpp::DataFrame edges) {
+    Rcpp::IntegerVector to = edges["to"];
+    Rcpp::IntegerVector from = edges["from"];
+    vector<double> input_cost = edges["cost"];
 
+    Graph G(nvertices);
+	vector<double> cost(nedges);
 
-pair< Graph, vector<double> > ReadWeightedGraph1(string filename)
-{
-	//Please see Graph.h for a description of the interface
+    for (int i = 0; i < nedges; i++)
+    {
+        G.AddEdge(from[i], to[i]);
+        cost[G.GetEdgeIndex(from[i], to[i])] = input_cost[i];
+    }
 
-	ifstream file;
-	file.open(filename.c_str());
-
-	string s;
-	getline(file, s);
-	stringstream ss(s);
-	int n;
-	ss >> n;
-	getline(file, s);
-	ss.str(s);
-	ss.clear();
-	int m;
-	ss >> m;
-
-	Graph G(n);
-	vector<double> cost(m);
-	for(int i = 0; i < m; i++)
-	{
-		getline(file, s);
-		ss.str(s);
-		ss.clear();
-		int u, v;
-		double c;
-		ss >> u >> v >> c;
-
-		G.AddEdge(u, v);
-		cost[G.GetEdgeIndex(u, v)] = c;
-	}
-
-	file.close();
-	return make_pair(G, cost);
+    return make_pair(G, cost);
 }
-
 
 //' @export
 // [[Rcpp::export]]
-Rcpp::List minimum_cost_perfect_matching(String filename) {
-    pair< Graph, vector<double> > p = ReadWeightedGraph1(filename);
+Rcpp::List minimum_cost_perfect_matching(int nvertices, int nedges, Rcpp::DataFrame edges) {
+    pair< Graph, vector<double> > p = CreateGraph(nvertices, nedges, edges);
 
 	Graph G = p.first;
 	vector<double> cost = p.second;
@@ -63,17 +34,17 @@ Rcpp::List minimum_cost_perfect_matching(String filename) {
 
 	list<int> matching = solution.first;
 
-	NumericMatrix edges(matching.size(), 2);
+	NumericMatrix matching_edges(matching.size(), 2);
 	int row_idx = 0;
 	for(list<int>::iterator it = matching.begin(); it != matching.end(); it++)
 	{
 		pair<int, int> e = G.GetEdge( *it );
 
-	    edges(row_idx, 0) = e.first;
-	    edges(row_idx, 1) = e.second;
+	    matching_edges(row_idx, 0) = e.first;
+	    matching_edges(row_idx, 1) = e.second;
 	    row_idx++;
 	}
 
     return Rcpp::List::create(Rcpp::Named("cost") = solution.second,
-                              Rcpp::Named("edges") = edges);
+                              Rcpp::Named("edges") = matching_edges);
 }
